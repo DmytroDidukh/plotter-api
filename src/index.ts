67e5665a732ecae6101c9c30fe073779da715740
dmytro_didukh <jeriko.outback@gmail.com>
2023-10-02 20:56:37 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,7 @@ app.use(rootRouter);
 
 // ERROR HANDLER
 app.use(errorHandlerMiddleware);
+// TEST COMMIT
 
 app.listen(config.PORT, () => {
     console.log(`SERVER IS STARTED ON ${config.PORT}`);
